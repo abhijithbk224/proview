7565da2525033cadbc12dfaded6bd3f83102d6d4
Kollu Charan <kollu.c@talview.com>
2025-07-24 11:25:15 +0530

    added sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
+import sitemap from "@astrojs/sitemap";
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://proview.io',
   vite: {
       plugins: [tailwindcss()]
 	},
 
-  integrations: [react()]
+  integrations: [react(),sitemap()]
 });
+
